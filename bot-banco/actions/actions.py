67b38654ff1eb53d0fc3c308964e25ca474771fe
Agustín Crespo <agustin.crespo22@gmail.com>
2022-08-29 12:15:09 -0300

    Connexion existosa a base de datos con clases

--- bot-banco/actions/actions.py
+++ bot-banco/actions/actions.py
@@ -1,13 +1,14 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from actions.db import dataInsert
+from actions.db import Client
 class ActionHelloWorld(Action):
 
 	def name(self) -> Text:
-		return "prueba_base"
+		return "action_saldo"
 
 	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dataInsert("Agustin", "Crespo")
+		cliente = Client()
+		print(cliente.select())
 		dispatcher.utter_message(text="Hello Worl!")
 		return []
--- bot-banco/actions/db.py
+++ bot-banco/actions/db.py
@@ -1,4 +1,43 @@
 import mysql.connector
+class Conneccion :
+	__db_host : str = "localhost"
+	__db_user : str = "agustin"
+	__db_password : str = "Agustin@22-23"
+	__db_database : str = "bot_banco"
+	def __init__(self) :
+		self.__conn = mysql.connector.connect(host = Conneccion.__db_host, username=Conneccion.__db_user, password=Conneccion.__db_password, database=Conneccion.__db_database)
+	def commit(self,sql):
+		cursor = self.__conn.cursor(buffered=True)
+		try:
+			cursor.execute(sql)
+			self.__conn.commit()
+			res = cursor.fetchall()
+		except mysql.connector.Error as e:
+			print ("Error code:", e.errno)        # error number
+			print ("SQLSTATE value:", e.sqlstate) # SQLSTATE value
+			print ("Error message:", e.msg)       # error message
+			print ("Error:", e)                   # errno, sqlstate, msg values
+			s = str(e)
+			print ("Error:", s)
+		return res
+
+class Client(Conneccion):
+	__table : str = "clientes"
+	__dni : str = "dni"
+	__nombre : str = "nombre"
+	__apellido : str = "apellido"
+	__cuenta : str = "cuenta"
+	__campos : str = __dni + ", " + __nombre + ", " + __apellido + ", " + __cuenta
+
+	def __init__(self) :
+		super().__init__()
+
+	def select(self):
+		res = Conneccion.commit(self, "SELECT " + Client.__campos + " FROM " + Client.__table)
+		return res
+
+
+"""
 def dataInsert(FirstName, LastName):
 	mydb = mysql.connector.connect(
 		host='localhost',
@@ -11,3 +50,4 @@ def dataInsert(FirstName, LastName):
 	sql = 'INSERT INTO tabla (nombre, apellido) VALUES ("{0}", "{1}");'.format(FirstName, LastName)
 	mycursor.execute(sql)
 	mydb.commit()
+"""
