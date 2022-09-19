ed54f0846c854c6f97bb995a5779f92c987ca507
Agustín Crespo <agustin.crespo22@gmail.com>
2022-09-19 17:38:47 -0300

    Cursadas actuales JSON

--- bot-yo-escuela/actions/actions.py
+++ bot-yo-escuela/actions/actions.py
@@ -1,14 +1,11 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from actions.estado_actual import EstadoActual
 
-class ActionHelloWorld(Action):
+class ActionGetCursadas(Action):
 	def name(self) -> Text:
-		return "action_nombre"
-
+		return "action_consulta_materias_en_curso"
 	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		retorno = next(tracker.get_latest_entity_values('nombre'), None)
-		print(retorno)
-		dispatcher.utter_message(text="Hello World!")
-
-		return []
+		materias = EstadoActual.getCursadas()
+		dispatcher.utter_message(text=materias)
---
+++ bot-yo-escuela/actions/estado_actual.py
@@ -0,0 +1,45 @@
+import random
+import os.path
+import json
+
+class OperarArchivo():
+	"""@staticmethod
+	def guardar(AGuardar):
+		with open(".\\actions\\datos","w") as archivo_descarga:
+		json.dump(AGuardar, archivo_descarga, indent=4)
+		archivo_descarga.close()"""
+
+	@staticmethod
+	def cargarArchivo(archivo): 
+		retorno = {}
+		if os.path.isfile(archivo):
+			with open(archivo,"r") as archivo_carga:
+				retorno=json.load(archivo_carga)
+				archivo_carga.close()
+		return retorno
+
+class EstadoActual:
+	datos = OperarArchivo.cargarArchivo("./actions/db/estado_actual.json")
+	@staticmethod
+	def getCursadas():
+		materias = EstadoActual.datos["materias"]["cursadas"]
+		retorno = "No estoy cursando nada" if(len(materias) == 0) else "Estoy cursando:\n"
+		for materia in materias:
+			retorno += f"- {materia}\n"
+		return retorno
+	@staticmethod	
+	def getFinales():
+		finales = EstadoActual.datos["finales"]["en_curso"]
+		retorno = "No estoy preparando ningun final" if(len(finales) == 0) else "Estoy preparando:\n\n"
+		for final in finales:
+			retorno += f"------------- {final['materia']} -------------\n"
+			retorno += f"- año: {str(final['año'])}\n"
+			retorno += f"- Cuatrimestre: {str(final['cuatrimestre'])}\n"
+			retorno += f"- Cursada Aprobada: " + "Si" if (final['cursada_aprobada']) else "No" + "\n"
+		return retorno
+	@staticmethod
+	def getAñoTeorico():
+		return EstadoActual.datos["año_teorico"]
+	@staticmethod
+	def getAñoIngreso():
+		return EstadoActual.datos["año_ingreso"]
