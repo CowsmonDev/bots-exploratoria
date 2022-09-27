0db070794d893df79bb2cc3de272ea446aae4118
Agustín Crespo <agustin.crespo22@gmail.com>
2022-09-27 11:50:09 -0300

    Area de interes Incompleta

--- bot-yo-escuela/actions/actions.py
+++ bot-yo-escuela/actions/actions.py
@@ -20,4 +20,18 @@ class ActionGetFinalActuales(Action):
 		return "action_consulta_finales"
 	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 		dispatcher.utter_message(text=EstadoActual.getFinales())
-		
+
+class ActionRamaInteres(Action):
+	def name(self)-> Text:
+		return "action_rama_interes"
+	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		if(next(tracker.get_latest_entity_values('tiempo', "universidad"), None) != None):
+			print("Presente - Universidad")
+		elif(next(tracker.get_latest_entity_values('tiempo', "secundaria"), None) != None):
+			print("Pasado - Secundaria")
+		else:
+			tiempo = next(tracker.get_latest_entity_values('tiempo', "general"), None)
+			if(tiempo == "pasado"):
+				print(str(tiempo))
+			else:
+				print(str(tiempo))
