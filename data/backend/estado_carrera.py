2f98678258b621128f0ed8e9d289923054a19f49
Agustín Crespo <55607741+CowsmonDev@users.noreply.github.com>
2022-11-21 12:20:20 -0300

    Yo Escuela (Sin Grupos)

---
+++ data/backend/estado_carrera.py
@@ -0,0 +1,113 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from data.backend.modules.estado_actual import EstadoActual
+from data.backend.modules.historia import Historia
+from rasa_sdk.events import SlotSet
+
+
+# TODO: Estado Actual
+class ActionGetAñoActual(Action):
+    def name(self) -> Text:
+        return "action_consultar_año"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text=f"uff, bueno... ahora mismo estoy en {str(EstadoActual.getAñoActual())}\n Igual deberia estar en {str(EstadoActual.getAñoTeorico())}")
+
+
+# TODO: action_consulta_finales_en_curso
+class ActionGetFinalActuales(Action):
+    def name(self) -> Text:
+        return "action_consulta_finales_en_curso"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getFinalesEnCurso())
+        return [SlotSet('slot_materia_estado', "finales")]
+
+
+# TODO: action_consulta_materias_en_curso
+class ActionGetCursadas(Action):
+    def name(self) -> Text:
+        return "action_consulta_materias_en_curso"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getCursadas())
+        return [SlotSet('slot_materia_estado', "materias")]
+
+
+# TODO: action_consulta_finales_pendientes
+class ActionGetFinalesPendientes(Action):
+    def name(self) -> Text:
+        return "action_consulta_finales_pendientes"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getFinalesPendientes())
+
+
+# TODO: action_consulta_estado_materia
+class ActionEstadoMateria(Action):
+    def name(self) -> Text:
+        return "action_consulta_estado_materia"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        materia = next(tracker.get_latest_entity_values('materia_estado'), None)
+        return [SlotSet('slot_materia_estado', materia)]
+
+
+# TODO: action_estas_cursando
+class ActionGetCursoTalMateria(Action):
+    def name(self):
+        return "action_estas_cursando"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        materia = next(tracker.get_latest_entity_values('materia_estado'), None)
+        if EstadoActual.estasCursando(materia):
+            dispatcher.utter_message(text=f"Si, la estoy cursando")
+            return [SlotSet('slot_materia_estado', materia)]
+        else:
+            dispatcher.utter_message(text=f"No... {EstadoActual.getCursadas()}")
+
+
+# TODO: action_como_venis_con_eso
+class ActionComoVenisConEso(Action):
+    def name(self) -> Text:
+        return "action_como_venis_con_eso"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[
+        Dict[Text, Any]]:
+        materia = tracker.get_slot("slot_materia_estado")
+        print(f"como venis con eso: {materia}")
+        if materia == "materias":
+            dispatcher.utter_message(
+                text="En general vengo bien, por ahora solo estoy esperando llegar a cumplir con todo")
+        elif materia == "finales":
+            dispatcher.utter_message(text="Honestamente, todavia no empece a estudiar")
+        elif materia is not None:
+            dispatcher.utter_message(text=str(EstadoActual.estadoMateria(materia)))
+        else:
+            dispatcher.utter_message(text="Perdon, de que estas hablando?")
+
+
+# TODO: Historia
+class ActionAprobados(Action):
+    def name(self) -> Text:
+        return "action_consulta_aprobados"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        res = next(tracker.get_latest_entity_values('materia_final'), None)
+        if res == "cursor":
+            dispatcher.utter_message(text=Historia.getMateriasAprobadas())
+        elif res == "final" or res == "finales":
+            dispatcher.utter_message(text=Historia.getFinalesAprobados())
+        else:
+            print(res)
+            dispatcher.utter_message(text="perdon no te entendi... hablas de finales o materias")
+
+
+class ActionGetTotalMaterias(Action):
+    def name(self) -> Text:
+        return "action_consulta_total_materias"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=Historia.getMaterias())
---
+++ data/backend/main.py
@@ -0,0 +1,75 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+from data.backend.modules.db_sql import PersonasDB
+
+
+class ActionSaludar(Action):
+    def name(self) -> Text:
+        return "action_saludar"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        from_message = tracker.latest_message["metadata"]['message']['from']
+        id_conversacion = from_message["id"]
+        persona = PersonasDB()
+        res = persona.existe_persona(str(id_conversacion))
+
+        if len(res) > 0:
+            nombre = res[0][2].split(' ')[0]
+            return [SlotSet('slot_nombre', nombre), SlotSet('slot_profesion', res[0][3]),
+                    SlotSet('slot_id_conversacion', id_conversacion)]
+        else:
+            dispatcher.utter_message(response="utter_saludar")
+        return []
+
+
+class ActionAhSi(Action):
+    def name(self) -> Text:
+        return "action_ah_si"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        profesion = tracker.get_slot("slot_profesion")
+        nombre = tracker.get_slot("slot_nombre")
+        id_conversacion = tracker.get_slot("slot_id_conversacion")
+        if profesion is None: profesion = "Null"
+        if nombre is None: nombre = "Null"
+
+        if id_conversacion is None:
+            id_conversacion = tracker.latest_message["metadata"]["message"]["from"]["id"]
+            persona = PersonasDB()
+            persona.agregar_persona([str(id_conversacion), "Null", str(nombre), str(profesion)])
+            return [SlotSet('slot_id_conversacion', id_conversacion)]
+
+        if profesion == "Profesor":
+            dispatcher.utter_message(text="que pasa profe?")
+        else:
+            if nombre == "Null":
+                dispatcher.utter_message(text="que pasa?")
+            else:
+                dispatcher.utter_message(text=f"ahh... si que pasa {nombre}")
+        return []
+
+
+class ActionComoEstas(Action):
+    def name(self) -> Text:
+        return "action_como_estas"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet('slot_tema_bot', 'robot')]
+
+
+class ActionDespedir(Action):
+    def name(self) -> Text:
+        return "action_despedir"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        profesion = tracker.get_slot("slot_profesion")
+        if profesion == "Profesor":
+            dispatcher.utter_message(text="nos vemos profe")
+        else:
+            nombre = tracker.get_slot("slot_nombre")
+            if nombre is None:
+                dispatcher.utter_message(text="nos vemos")
+            else:
+                dispatcher.utter_message(text=f"nos vemos {nombre}")
