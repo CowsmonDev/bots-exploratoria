d591a2f62d4cc590825b7cac0bb3794e32cc9465
Agustín Crespo <agustin.crespo22@gmail.com>
2022-11-21 10:00:01 -0300

    Implementacion Nueva Estructura

--- data/backend/estado_carrera.py
+++ data/backend/estado_carrera.py
@@ -5,91 +5,109 @@ from data.backend.modules.estado_actual import EstadoActual
 from data.backend.modules.historia import Historia
 from rasa_sdk.events import SlotSet
 
+
 # TODO: Estado Actual
 class ActionGetAñoActual(Action):
-	def name(self)-> Text:
-		return "action_consultar_año"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dispatcher.utter_message(text=f"uff, bueno... ahora mismo estoy en {str(EstadoActual.getAñoActual())}\n Igual deberia estar en {str(EstadoActual.getAñoTeorico())}")
+    def name(self) -> Text:
+        return "action_consultar_año"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text=f"uff, bueno... ahora mismo estoy en {str(EstadoActual.getAñoActual())}\n Igual deberia estar en {str(EstadoActual.getAñoTeorico())}")
+
 
 # TODO: action_consulta_finales_en_curso
 class ActionGetFinalActuales(Action):
-	def name(self)-> Text:
-		return "action_consulta_finales_en_curso"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dispatcher.utter_message(text=EstadoActual.getFinalesEnCurso())
-		return [SlotSet('slot_materia_estado', "finales")]
+    def name(self) -> Text:
+        return "action_consulta_finales_en_curso"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getFinalesEnCurso())
+        return [SlotSet('slot_materia_estado', "finales")]
+
 
 # TODO: action_consulta_materias_en_curso
 class ActionGetCursadas(Action):
-	def name(self) -> Text:
-		return "action_consulta_materias_en_curso"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dispatcher.utter_message(text=EstadoActual.getCursadas())
-		return [SlotSet('slot_materia_estado', "materias")]
+    def name(self) -> Text:
+        return "action_consulta_materias_en_curso"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getCursadas())
+        return [SlotSet('slot_materia_estado', "materias")]
+
 
 # TODO: action_consulta_finales_pendientes
 class ActionGetFinalesPendientes(Action):
-	def name(self)-> Text:
-		return "action_consulta_finales_pendientes"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dispatcher.utter_message(text=EstadoActual.getFinalesPendientes())
+    def name(self) -> Text:
+        return "action_consulta_finales_pendientes"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=EstadoActual.getFinalesPendientes())
+
 
 # TODO: action_consulta_estado_materia
 class ActionEstadoMateria(Action):
-	def name(self) -> Text:
-		return "action_consulta_estado_materia"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		materia = next(tracker.get_latest_entity_values('materia_estado'), None)
-		return [SlotSet('slot_materia_estado', materia)]
+    def name(self) -> Text:
+        return "action_consulta_estado_materia"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        materia = next(tracker.get_latest_entity_values('materia_estado'), None)
+        return [SlotSet('slot_materia_estado', materia)]
+
 
 # TODO: action_estas_cursando
 class ActionGetCursoTalMateria(Action):
-	def name(self):
-		return "action_estas_cursando"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		materia = next(tracker.get_latest_entity_values('materia_estado'), None)
-		if(EstadoActual.estasCursando(materia)):
-			dispatcher.utter_message(text=f"Si, la estoy cursando")
-			return [SlotSet('slot_materia_estado', materia)]
-		else:
-			dispatcher.utter_message(text=f"No... {EstadoActual.getCursadas()}")
+    def name(self):
+        return "action_estas_cursando"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        materia = next(tracker.get_latest_entity_values('materia_estado'), None)
+        if EstadoActual.estasCursando(materia):
+            dispatcher.utter_message(text=f"Si, la estoy cursando")
+            return [SlotSet('slot_materia_estado', materia)]
+        else:
+            dispatcher.utter_message(text=f"No... {EstadoActual.getCursadas()}")
+
 
 # TODO: action_como_venis_con_eso
 class ActionComoVenisConEso(Action):
-	def name(self) -> Text:
-		return "action_como_venis_con_eso"
-	def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[
-		Dict[Text, Any]]:
-		materia = tracker.get_slot("slot_materia_estado")
-		print(f"como venis con eso: {materia}")
-		if materia == "materias":
-			dispatcher.utter_message(text="En general vengo bien, por ahora solo estoy esperando llegar a cumplir con todo")
-		elif materia == "finales":
-			dispatcher.utter_message(text="Honestamente, todavia no empece a estudiar")
-		elif materia is not None:
-			dispatcher.utter_message(text=str(EstadoActual.estadoMateria(materia)))
-		else:
-			dispatcher.utter_message(text="Perdon, de que estas hablando?")
+    def name(self) -> Text:
+        return "action_como_venis_con_eso"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[
+        Dict[Text, Any]]:
+        materia = tracker.get_slot("slot_materia_estado")
+        print(f"como venis con eso: {materia}")
+        if materia == "materias":
+            dispatcher.utter_message(
+                text="En general vengo bien, por ahora solo estoy esperando llegar a cumplir con todo")
+        elif materia == "finales":
+            dispatcher.utter_message(text="Honestamente, todavia no empece a estudiar")
+        elif materia is not None:
+            dispatcher.utter_message(text=str(EstadoActual.estadoMateria(materia)))
+        else:
+            dispatcher.utter_message(text="Perdon, de que estas hablando?")
+
 
 # TODO: Historia
 class ActionAprobados(Action):
-	def name(self)-> Text:
-		return "action_consulta_aprobados"
+    def name(self) -> Text:
+        return "action_consulta_aprobados"
 
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		res = next(tracker.get_latest_entity_values('materia_final'), None)
-		if res == "cursor":
-			dispatcher.utter_message(text=Historia.getMateriasAprobadas())
-		elif res == "final" or res == "finales":
-			dispatcher.utter_message(text=Historia.getFinalesAprobados())
-		else:
-			print(res)
-			dispatcher.utter_message(text="perdon no te entendi... hablas de finales o materias")
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        res = next(tracker.get_latest_entity_values('materia_final'), None)
+        if res == "cursor":
+            dispatcher.utter_message(text=Historia.getMateriasAprobadas())
+        elif res == "final" or res == "finales":
+            dispatcher.utter_message(text=Historia.getFinalesAprobados())
+        else:
+            print(res)
+            dispatcher.utter_message(text="perdon no te entendi... hablas de finales o materias")
+
 
 class ActionGetTotalMaterias(Action):
-	def name(self) -> Text:
-		return "action_consulta_total_materias"
-	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-		dispatcher.utter_message(text=Historia.getMaterias())
+    def name(self) -> Text:
+        return "action_consulta_total_materias"
 
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=Historia.getMaterias())
--- data/backend/modules/db_mongo.py
+++ data/backend/modules/db_mongo.py
@@ -1,43 +1,44 @@
 from pymongo import MongoClient
 
+
 class SingletonDB(object):
+    __hostname: str = 'localhost'
+    __port: int = 27017
 
-	__hostname : str = 'localhost'
-	__port : int = 27017
+    def __init__(self):
+        self.__client = MongoClient(self.__hostname, self.__port)
 
-	def __init__(self):
-		self.__client = MongoClient(self.__hostname, self.__port)
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SingletonDB, cls).__new__(cls)
+        return cls.instance
 
-	def __new__(cls):
-		if not hasattr(cls, 'instance'):
-			cls.instance = super(SingletonDB, cls).__new__(cls)
-		return cls.instance
+    def getClient(self):
+        return self.__client
 
-	def getClient(self):
-		return self.__client
 
 class PersonasDB:
-	
-	def __init__(self):
-		self.__singleton_db = SingletonDB()
-		self.__db = self.__singleton_db.getClient().bot_rasa
-		self.__collection = self.__db.personas
 
-	def insertData(self, dato):
+    def __init__(self):
+        self.__singleton_db = SingletonDB()
+        self.__db = self.__singleton_db.getClient().bot_rasa
+        self.__collection = self.__db.personas
 
-		self.__collection.update(
-			{
-				"name" : dato[0]
-			}, 
-			{
-				"$setOnInsert": dato
-			},
-		)
-	def updateData(self, dato):
-		self.__collection.findAndModify()
-	
-	def find(self, dato):
-		res = self.__collection.find({
-			"name" : dato.name
-		})
-		return res
+    def insertData(self, dato):
+        self.__collection.update(
+            {
+                "name": dato[0]
+            },
+            {
+                "$setOnInsert": dato
+            },
+        )
+
+    def updateData(self, dato):
+        self.__collection.findAndModify()
+
+    def find(self, dato):
+        res = self.__collection.find({
+            "name": dato.name
+        })
+        return res
